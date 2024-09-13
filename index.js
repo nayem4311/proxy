5e95917ff21f976682c828574c093a36256a9b2e
M. NAYEM <119791098+nayem4311@users.noreply.github.com>
2024-09-13 12:06:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
-// Middleware to enable CORS
+// Middleware to enable CORS for all origins
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
